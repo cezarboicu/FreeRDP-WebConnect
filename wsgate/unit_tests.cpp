fd2bf5fd835ce8f37baf7735c7ce4c2945c43340
Cezar Boicu <cboicu@cloudbasesolutions.com>
2014-04-09 08:18:35 -0400

    added first unit tests file

---
+++ wsgate/unit_tests.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE wsgate_unit_tests
+#include <boost/test/unit_test.hpp>
+#include <base64.hpp>
+
+BOOST_AUTO_TEST_CASE(test1)
+{
+
+BOOST_REQUIRE(true);
+}
+
+
+
+

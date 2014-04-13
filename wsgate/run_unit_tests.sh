a2d3b0d427662cff90e51ce7c25c27ddaf2388b7
Cezar Boicu <cboicu@cloudbasesolutions.com>
2014-04-13 10:58:53 +0300

    extracted first class - myrawsocket from wsgate main and placed it in a different file

--- wsgate/run_unit_tests.sh
+++ wsgate/run_unit_tests.sh
@@ -1,2 +1,2 @@
-g++ unit_tests.cpp -o wsgate_unit_tests -lboost_unit_test_framework
+g++ unit_tests.cpp -o wsgate_unit_tests -DHAVE_CONFIG_H -I. -DBINDHELPER_PATH=\"/usr/local/libexec/wsgate/bindhelper\" -DDEFAULTCFG=\"/usr/local/etc/wsgate.ini\" -I/usr/include/casablanca -I/usr/include/ehs -g -fno-strict-aliasing  -std=c++11 -lboost_unit_test_framework
 ./wsgate_unit_tests
--- wsgate/unit_tests.cpp
+++ wsgate/unit_tests.cpp
@@ -1,13 +1,31 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE wsgate_unit_tests
 #include <boost/test/unit_test.hpp>
+#include "RDP.hpp"
+#include "Png.hpp"
 
-BOOST_AUTO_TEST_CASE(test1)
+BOOST_AUTO_TEST_SUITE(rdp_cpp_test_suite);
+
+BOOST_AUTO_TEST_CASE(s1_test1)
 {
-
-BOOST_REQUIRE(true);
+//	wspp::MyWsHandler *ws = NULL;
+//wsgate::RDP *rdp_instance = new wsgate::RDP(ws);
+BOOST_CHECK(true);
 }
 
+BOOST_AUTO_TEST_SUITE_END();
 
 
+
+BOOST_AUTO_TEST_SUITE(png_cpp_test_suite);
+
+BOOST_AUTO_TEST_CASE(s2_test1)
+{
+wsgate::Png png_instance;
+int w = 10;
+int h = 10;
+
+}
+
+BOOST_AUTO_TEST_SUITE_END();
 
